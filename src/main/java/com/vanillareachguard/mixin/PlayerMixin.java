fa044a529a12af462b53cec7887f87a489eff114
RTM <raffertymoyle@gmail.com>
2026-05-09 23:29:51 +0100

    Update PlayerMixin.java

--- src/main/java/com/vanillareachguard/mixin/PlayerMixin.java
+++ src/main/java/com/vanillareachguard/mixin/PlayerMixin.java
@@ -1,6 +1,6 @@
-package com.raffe.vanillareachguard.mixin;
+package com.vanillareachguard.mixin;
 
-import com.raffe.vanillareachguard.ReachGuard;
+import com.vanillareachguard.ReachGuard;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
